7a45c00c1a41332aafd6c8f89c3fcdf9913ffbe6
Alain Villegas <alain@MacBook-Air-de-Alain.local>
2026-06-07 03:28:25 -0600

    fix: serve web files from backend/public folder

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../web')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, {
   realtime: { transport: ws }
